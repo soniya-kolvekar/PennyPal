72dc8b3df342bc548f4d18054e894d047195cd57
zaynahanjum <zaynahanjum789@gmail.com>
2026-09-19 15:02:41 +0530

    implemented parser

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBrKnM5kJIWg12dYB313jCzIuQq9s5j2Mc",
-  authDomain: "pennypal-b2a1f.firebaseapp.com",
-  projectId: "pennypal-b2a1f",
-  storageBucket: "pennypal-b2a1f.firebasestorage.app",
-  messagingSenderId: "22395767897",
-  appId: "1:22395767897:web:df11f900878d3885017548"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 module.exports = { firebaseConfig };
